f26373a49d740b21f48bba91989a2dec4a31f11b
mtymes <matejtymes@gmail.com>
2018-07-15 01:39:15 +0200

    mtymes: verifying Garden's parameters

--- src/main/java/ms/garden/Garden.java
+++ src/main/java/ms/garden/Garden.java
@@ -1,8 +1,12 @@
 package ms.garden;
 
+import com.google.common.base.Preconditions;
+
 import java.util.Collection;
 import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.stream.Collectors.toList;
 import static javafixes.common.CollectionUtil.newSet;
 
@@ -10,10 +14,20 @@ public class Garden {
     private final int[][] map;
 
     public Garden(int[][] map) {
-        // todo: clone the map
-        // todo: check the map is of rectangular shape
+        checkNotNull(map, "map can't be null");
+        checkArgument(map.length > 0, "map can't be empty");
 
-        this.map = map;
+        int rowSize = map[0].length;
+        int mapCopy[][] = new int[map.length][];
+        for (int i = 0; i < map.length; i++) {
+            checkArgument(map[i].length > 0, "row map[" + i + "] can't be empty");
+            checkArgument(map[i].length == rowSize, "map isn't of a rectangular shape");
+
+            mapCopy[i] = new int[map[i].length];
+            System.arraycopy(map[i], 0, mapCopy[i], 0, map[i].length);
+        }
+
+        this.map = mapCopy;
     }
 
     public int getWidth() {
